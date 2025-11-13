353353a67fa90696f91dfa9cd6e2e969f284871a
Joseph Kaplan <u1364550@umail.utah.edu>
2025-11-13 01:11:47 -0700

    Create NetworkCondition.h
    Created a struct to represent various network conditions

---
+++ NetworkCondition.h
@@ -0,0 +1,13 @@
+#ifndef NETWORKCONDITION_H
+#define NETWORKCONDITION_H
+
+struct NetworkCondition 
+{
+float latencyMs;
+float jitterMs;
+float packetLoss;
+}
+
+// maybe have a randomize method???
+
+#endif NETWORKCONDITION_H
